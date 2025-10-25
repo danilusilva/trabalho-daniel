5a1e2019cd5724faad385d3b120202bf7fa1e74d
Danilu Silva <silvadanilu06@gmail.com>
2025-10-25 15:27:13 -0300

    feat: conexão com o MongoDB

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const conectarMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Conectado ao MongoDB');
+    } catch (err) {
+        console.error('Erro ao conectar ao MongoDB:', err);
+
+        // Opção nova de encerrar o processo em caso de falha na conexão (Eu acabei de descobrir, ok Daniel?)
+        process.exit(1);
+    }
+};
+
+module.exports = conectarMongoDB;
