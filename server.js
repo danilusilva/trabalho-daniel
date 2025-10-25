91214ac370b5221670d8fcec1b2a56b062fad62a
Danilu Silva <silvadanilu06@gmail.com>
2025-10-25 15:32:54 -0300

    feat: atualização no arquivo server.js

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const app = require('./src/app.js'); // Importando a configuração do Express
+const conectarMongoDB = require('./src/config/database.js'); // Importando a função de conexão com o MongoDB
 
-const app = express();
+// Inicia conexão com o MongoDB
+conectarMongoDB();
 
+// Inicia o servidor na porta definida no .env ou na porta 3000 por padrão
+const PORT = process.env.PORT || 3000;
 
-// Middleware que permite que o servidor entenda requisições com corpo em JSON
-app.use(express.json());
-
-
-// Conexão com o banco de dados MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Conectado ao MongoDB'))
-    .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
-
-// Rota de teste
-app.get('/', (req, res) =>{
-    res.status(200)
-    .json({ message: 'Servidor rodando com sucesso!'});
-})
-
-// Inicia o servidor
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 })
